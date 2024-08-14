307c5c3646b746974947dc8451885f7afbdb6de7
Sarath Mohan <sarathmohan.kk@gmail.com>
2024-08-14 22:44:11 +0530

    Added helper codes

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '1.0.0',
+    author= 'Sarath Mohan',
+    author_email= 'sarathmohan.kk@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
